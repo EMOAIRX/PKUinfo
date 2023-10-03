1567e7d4b3984ab3c65ca61f8f9ad54fbce13234
emoairx <emoairx@gmail.com>
2023-10-03 07:41:13 +0000

    change submodule

--- front/src/utils/request.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-// 封装 baseURL
-const request = axios.create({
-    baseURL: "https://pkuinfo.lcpu.dev/api"
-    // baseURL: "http://localhost:8080/api"
-})
-// 向外暴露 request
-export default request;
